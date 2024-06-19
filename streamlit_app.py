08d86183a299250129765b3d0b2bcdcc0be7191e
chinnivaishnaviduvvapu <165121433+chinnivaishnaviduvvapu@users.noreply.github.com>
2024-06-19 14:31:23 +0530

    Update streamlit_app.py
    Let's Get the Fruityvice Data to Show Data for the Fruits Chosen
    To do this, we need to rearrange our code lines.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,12 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
+import requests
 # Write directly to the app
 st.title(":cup_with_straw: Customise your Smoothie :cup_with_straw:")
 st.write(
     """choose the fruits you want in your custom smoothie!!
-    """
-)
+    """)
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your smoothie will be", name_on_order)
@@ -19,22 +18,15 @@ ingredients_list= st.multiselect('choose up to five ingredients:',my_dataframe,m
 if ingredients_list:
     # st.write(ingredients_list)
     # st.text(ingredients_list)
-
     ingredients_stirng=''
     for i in ingredients_list:
         ingredients_stirng+=i+' '
-
-    
-    # st.write(ingredients_stirng)
-
-    
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+        # st.write(ingredients_stirng)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_stirng + """','""" + name_on_order + """')"""
-
-    
     # st.write(my_insert_stmt) 
-
-    
     time_to_insert = st.button('Submit Order')
 
     
@@ -45,9 +37,7 @@ if ingredients_list:
         st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
 
 # Let's Call the Fruityvice API from Our SniS App!We need to bring in a Python package library called requests.  The requests library allows us to build and sent REST API calls. 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
 
 # Let's Put the JSON into a Dataframe.People often use df as shorthand for "dataframe." We'll call our dataframe fv_df, because it's our Fruityvice Dataframe.
-fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+
