fc8aaf77e748cc25dc6ef7aa3b60dd0ab51c2165
chinnivaishnaviduvvapu <165121433+chinnivaishnaviduvvapu@users.noreply.github.com>
2024-06-19 16:01:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,13 @@ st.write("The name on your smoothie will be", name_on_order)
 cnx=st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()                                                                      
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()   
+
+pd_df=my_dataframe.to_pandas()
+#st.dataframe(pd_df)
+#st.stop()
+
 ingredients_list= st.multiselect('choose up to five ingredients:',my_dataframe,max_selections=5)
 if ingredients_list:
     # st.write(ingredients_list)
@@ -23,6 +28,8 @@ if ingredients_list:
     ingredients_stirng=''
     for i in ingredients_list:
         ingredients_stirng+=i+' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == i, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', i,' is ', search_on, '.')
         st.subheader(i+' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+i)
         fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
